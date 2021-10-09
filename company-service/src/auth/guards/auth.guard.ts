1ea42b478ac305f3914cee1e399ae6cb3391d759
Kaiwen Luo <luokw8@gmail.com>
2021-10-09 22:08:39 +1100

    :passport_control: fix: return 401 error if no JWT

--- company-service/src/auth/guards/auth.guard.ts
+++ company-service/src/auth/guards/auth.guard.ts
@@ -4,23 +4,29 @@ import {
   Inject,
   Injectable,
   RequestTimeoutException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { timeout, catchError } from 'rxjs/operators';
-import { TimeoutError, throwError } from 'rxjs';
+import { lastValueFrom, TimeoutError, throwError } from 'rxjs';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class JwtAuthGuard implements CanActivate {
   constructor(
     @Inject('AUTH_CLIENT')
     private readonly client: ClientProxy,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const req = context.switchToHttp().getRequest();
+    const req = context.switchToHttp().getRequest<Request>();
+    const jwt = req.headers['authorization']?.split(' ')[1];
+
+    if (!jwt) {
+      throw new UnauthorizedException();
+    }
 
     try {
-      const user = await this.client
+      const user$ = this.client
         .send(
           { role: 'auth', cmd: 'check' },
           { jwt: req.headers['authorization']?.split(' ')[1] },
@@ -29,14 +35,18 @@ export class AuthGuard implements CanActivate {
           timeout(5000),
           catchError((err) => {
             if (err instanceof TimeoutError) {
-              return throwError(new RequestTimeoutException());
+              return throwError(() => new RequestTimeoutException());
             }
-            return throwError(err);
+            return throwError(() => err);
           }),
-        )
-        .toPromise();
+        );
+      // toPromise is deprecated
+      const user = await lastValueFrom(user$);
 
-      return user && true;
+      if (user) {
+        return true;
+      }
+      return false;
     } catch (err) {
       console.log(err);
       return false;
--- company-service/src/companies/companies.controller.ts
+++ company-service/src/companies/companies.controller.ts
@@ -1,25 +1,25 @@
 import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { CompaniesService } from './companies.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
-import { AuthGuard } from './guards/auth.guard';
+import { JwtAuthGuard } from '@@auth/guards/auth.guard';
 
 @Controller('companies')
 export class CompaniesController {
   constructor(private readonly companiesService: CompaniesService) {}
 
-  @UseGuards(AuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createCompanyDto: CreateCompanyDto) {
     return this.companiesService.create(createCompanyDto);
   }
 
-  @UseGuards(AuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.companiesService.findAll();
   }
 
-  @UseGuards(AuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.companiesService.findById(id);
